8f52ab849899a02766eec0bf16f50977af7ae787
Cmmatrubai <chaitanya.matrubai@gmail.com>
2024-11-23 17:07:35 -0500

    More ui changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
 import '../models/article.dart';
+import '../models/featured_news_model.dart';
+import '../models/news_category_model.dart';
 import '../services/api_service.dart';
 import '../services/hive_service.dart';
-import 'package:flutter_svg/svg.dart';
-import '../models/news_category_model.dart';
-import '../models/featured_news_model.dart';
-import 'business_articles_page.dart';
-import 'tech_articles_page.dart';
+
 import 'category_articles_page.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -70,16 +71,15 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
         appBar: _buildAppBar(),
+        backgroundColor: Colors.white,
         body: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 20),
           children: [
+            _searchField(),
+            const SizedBox(height: 20),
             _categoriesSection(),
-            const SizedBox(height: 40),
-            _featuredSection(),
-            const SizedBox(height: 40),
-            _latestNewsSection(),
+            const SizedBox(height: 20),
+            _popularSection(),
           ],
         ),
       ),
@@ -88,33 +88,55 @@ class _HomePageState extends State<HomePage> {
 
   AppBar _buildAppBar() {
     return AppBar(
-      title: const Text('News App'),
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(56.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            controller: _searchController,
-            onSubmitted: _searchArticles,
-            decoration: InputDecoration(
-              hintText: 'Search Articles',
-              fillColor: Colors.white,
-              filled: true,
-              prefixIcon: const Icon(Icons.search),
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.clear),
-                onPressed: () {
-                  _searchController.clear();
-                  _searchArticles('');
-                },
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
+      title: const Padding(
+        padding: EdgeInsets.only(top: 30),
+        child: Text(
+          'The Daily Targum',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+          ),
+        )
+      ],
     );
   }
 
@@ -122,7 +144,6 @@ class _HomePageState extends State<HomePage> {
     final categories = [
       NewsCategoryModel(name: 'Business', iconPath: 'assets/icons/business.svg'),
       NewsCategoryModel(name: 'Entertainment', iconPath: 'assets/icons/entertainment.svg'),
-      NewsCategoryModel(name: 'General', iconPath: 'assets/icons/general.svg'),
       NewsCategoryModel(name: 'Health', iconPath: 'assets/icons/health.svg'),
       NewsCategoryModel(name: 'Science', iconPath: 'assets/icons/science.svg'),
       NewsCategoryModel(name: 'Sports', iconPath: 'assets/icons/sports.svg'),
@@ -137,7 +158,7 @@ class _HomePageState extends State<HomePage> {
           child: Text(
             'Categories',
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 18,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -157,31 +178,154 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _featuredSection() {
-    return SizedBox(
-      height: 250,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: featuredNews.length,
-        itemBuilder: (context, index) {
-          return FeaturedNewsItem(news: featuredNews[index]);
-        },
+  Container _searchField() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xff1D1617).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0,
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: _searchController,
+        onSubmitted: _searchArticles,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'Search News',
+          hintStyle: const TextStyle(
+            color: Color(0xffDDDADA),
+            fontSize: 14,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset('assets/icons/Search.svg'),
+          ),
+          suffixIcon: SizedBox(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const VerticalDivider(
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: SvgPicture.asset('assets/icons/Filter.svg'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+        ),
       ),
     );
   }
 
-  Widget _latestNewsSection() {
-    return SizedBox(
-      height: 250,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _articles.length,
-        itemBuilder: (context, index) {
-          return ArticleItem(article: _articles[index]);
-        },
-      ),
+  Column _popularSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Trending',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        Container(
+          height: 240,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            itemCount: _articles.length,
+            separatorBuilder: (context, index) => const SizedBox(width: 25),
+            itemBuilder: (context, index) {
+              final article = _articles[index];
+              return Container(
+                width: 210,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: article.urlToImage != null && article.urlToImage!.isNotEmpty
+                            ? Image.network(
+                                article.urlToImage!,
+                                height: 100,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              )
+                            : Container(
+                                height: 100,
+                                color: Colors.grey,
+                                child: const Center(child: Icon(Icons.image)),
+                              ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              article.title ?? 'No Title',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              article.description ?? 'No Description',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff7B6F72),
+                                fontSize: 13,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
+
 }
 
 class FeaturedNewsItem extends StatelessWidget {
